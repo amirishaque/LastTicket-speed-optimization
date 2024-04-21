8c36fd7e3335efdd9c28873097a673a71649265e
Hamza Haseeb <hamzahaseeb1996@gmail.com>
2024-04-22 00:32:34 +0500

    Fixes

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -4,14 +4,12 @@ import "./style/style.css";
 const Slider = ({ text }) => {
   const [isPaused, setIsPaused] = useState(false);
   const marqueeRef = useRef(null);
-  const [startX, setStartX] = useState(0);
-  const [startScroll, setStartScroll] = useState(0);
   const [loopedText, setLoopedText] = useState("");
 
   useEffect(() => {
-    // Concatenate the text to itself to create a loop
-    setLoopedText(text + " " + text);
-    
+    // Concatenate the text to itself to create a loop without any additional spaces
+    setLoopedText(text + text);
+
     // Start the animation immediately when the component mounts
     const marquee = marqueeRef.current;
     marquee.classList.remove("paused");
@@ -20,40 +18,24 @@ const Slider = ({ text }) => {
   const handleMouseOver = () => setIsPaused(true);
   const handleMouseOut = () => setIsPaused(false);
 
-  const handleMouseDown = (e) => {
-    setStartX(e.pageX);
-    setStartScroll(marqueeRef.current.scrollLeft);
-    setIsPaused(true); // Pause animation on drag start
-    document.addEventListener("mousemove", handleMouseMove);
-    document.addEventListener("mouseup", handleMouseUp);
-  };
-
-  const handleMouseMove = (e) => {
-    const dx = e.pageX - startX;
-    marqueeRef.current.scrollLeft = startScroll - dx;
-  };
-
-  const handleMouseUp = () => {
-    document.removeEventListener("mousemove", handleMouseMove);
-    document.removeEventListener("mouseup", handleMouseUp);
-    setIsPaused(false); // Resume animation after drag
+  const handleAnimationEnd = () => {
+    // When animation ends, reset scroll position to seamlessly transition to the beginning of the loop
+    const marquee = marqueeRef.current;
+    marquee.scrollLeft = 0;
   };
 
   return (
-    <>
-      <div className="slider-wrapper">
-        <div
-          ref={marqueeRef}
-          className={`marquee ${isPaused ? "paused" : ""}`}
-          onMouseOver={handleMouseOver}
-          onMouseOut={handleMouseOut}
-          onMouseDown={handleMouseDown}
-        >
-          {/* {loopedText} */}
-          Hurray! You are in the right place *  100K+ social media followers  *   We value every customer *  100% customer satisfaction   * We guarantee your entry *  SSL certificate website *  Secured payout  *  24/7 Customer Support* If you are not 100% satisfied, we will refund you 100% 
-        </div>
+    <div className="slider-wrapper">
+      <div
+        ref={marqueeRef}
+        className={`marquee ${isPaused ? "paused" : ""}`}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+        onAnimationIteration={handleAnimationEnd}
+      >
+        Hurray! You are in the right place *  100K+ social media followers  *   We value every customer *  100% customer satisfaction   * We guarantee your entry *  SSL certificate website *  Secured payout  *  24/7 Customer Support* If you are not 100% satisfied, we will refund you 100% 
       </div>
-    </>
+    </div>
   );
 };
 
