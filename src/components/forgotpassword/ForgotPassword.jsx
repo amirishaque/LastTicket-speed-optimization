117b997ab3fef6d0a5f185283b6f28105eb735e3
Hamza Haseeb <hamzahaseeb1996@gmail.com>
2024-05-17 15:45:09 +0500

    fixed

--- src/components/forgotpassword/ForgotPassword.jsx
+++ src/components/forgotpassword/ForgotPassword.jsx
@@ -5,9 +5,9 @@ export default function ForgotPassword({ toggleForm }) {
         <>
             <div className="login-inner-wrapper">
                 <h1>Forgot Password</h1>
-                <form style={{maxWidth:'358px', margin:'0 auto', padding:'30px'}}>
+                <form style={{maxWidth:'358px', margin:'0 auto 0 0', padding:'30px'}}>
                     <input type="text" placeholder="User Name or Email" style={{ maxWidth: '385px' }} />
-                    <button type="submit" style={{width:'384px'}}>Reset Password</button>
+                    <button type="submit" style={{width:'384px'}}>Proceed with password reset</button>
                     <p><span>Remember your password?</span> <button onClick={toggleForm}>Login here</button></p>
                 </form>
                 {/* <p>Remember your password? <button onClick={toggleForm}>Login here</button></p> */}
